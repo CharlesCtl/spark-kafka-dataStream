06a81fd418a054b963af99e6cec73fda0a8c1d1e
Carlos Catalan <charlesing92@gmail.com>
2024-06-26 10:03:55 -0600

    Added multithreading python producer, data in send to kafka correctly

--- python-producer/main.py
+++ python-producer/main.py
@@ -1,15 +1,40 @@
 from kafka import KafkaProducer
 from kafka.errors import KafkaError
+import json
 import time
+import threading
 
-if __name__=="__main__":
+def init_producer():
     time.sleep(10)
-    print("Hello from python container")
-    producer = KafkaProducer(bootstrap_servers=['kafka-server-2:9092'])
+    return KafkaProducer(bootstrap_servers=['kafka-server-2:9092'],
+                             value_serializer=lambda v: json.dumps(v).encode('utf-8'))
+def send_message(topic,file,key=None):
+    count = 0
+    for invoice in read_json(file):
+        message = {key: invoice}
+        producer.send(topic,message)
+        producer.flush()
+        count += 1
+        if count == 10:
+            break
+    return True
+def read_json(file_path):
+    with open(file_path,'r') as file:
+        for line in file:
+            yield json.loads(line)
+
+file1 = 'invoices_data/invoices-1.json'
+file2 = 'invoices_data/invoices-2.json'
+topic = 'test'
+if __name__=="__main__":
+    producer = init_producer()
     if producer.bootstrap_connected():
-        for _ in range(2):
-            message = b'Hello, Kafka!'
-            producer.send('test', message)
+        thread1 = threading.Thread(target=send_message,args=(topic,file1,1,))
+        thread2 = threading.Thread(target=send_message,args=(topic,file2,2,))
+        thread1.start()
+        thread2.start()
+        thread1.join()
+        thread1.join()
+        producer.close()
     else:
         print('Failed connection')
-    producer.close()    
